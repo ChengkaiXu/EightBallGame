0d16e83ce953a7b7e971e31538aceebbfb949bfb
KaiXu2195 <137818241+KaiXu2195@users.noreply.github.com>
2023-12-01 09:19:49 -0800

    Fixing problems

--- core/src/xu/chengkai/eightball/EightBallGame.java
+++ core/src/xu/chengkai/eightball/EightBallGame.java
@@ -2,10 +2,12 @@ package xu.chengkai.eightball;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
@@ -18,48 +20,43 @@ import java.awt.*;
 
 public class EightBallGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	BitmapFont font;
-	Label label;
-	Stage stage;
+	Camera camera = new Camera() {
+		@Override
+		public void update() {
+
+		}
+
+		@Override
+		public void update(boolean updateFrustum) {
+
+		}
+	};
 	TextButton button;
+	PoolTable poolTable;
+	Box2DDebugRenderer renderer;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		font = new BitmapFont();
-		label = new Label("Daizi", new Label.LabelStyle(font, Color.WHITE));
-		label.setPosition(500, 400);
-		button = new TextButton("XiaoChouZiv", new TextButton.TextButtonStyle(null, null, null, font));
-		button.setPosition(500, 500);
-		stage = new Stage();
-		stage.addActor(label);
-		stage.addActor(button);
-		button.addListener(new ChangeListener() {
-			@Override
-			public void changed(ChangeEvent event, Actor actor) {
-				button.setText("DabianZiv");
-			}
-		});
-		Gdx.input.setInputProcessor(stage);
+
+		poolTable = new PoolTable();
+
+		Gdx.input.setInputProcessor(poolTable);
+
+		renderer = new Box2DDebugRenderer();
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
-		stage.draw();
-		stage.act();
 		batch.begin();
-		batch.draw(img, 0, 0);
-		font.getData().setScale(10);
-		font.draw(batch, "DaiziLeo", 100, 100);
+		renderer.render(poolTable.getWorld(), camera.combined);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		renderer.dispose();
 	}
 }
--- core/src/xu/chengkai/eightball/PoolTable.java
+++ core/src/xu/chengkai/eightball/PoolTable.java
@@ -1,5 +1,6 @@
 package xu.chengkai.eightball;
 
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -9,7 +10,7 @@ import com.badlogic.gdx.physics.box2d.World;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PoolTable {
+public class PoolTable implements InputProcessor {
     World world = new World(new Vector2(0, 0), true);
     List<Ball> stripeBalls = new ArrayList<>();
     List<Ball> solidBalls = new ArrayList<>();
@@ -30,5 +31,54 @@ public class PoolTable {
         PolygonShape groundBox = new PolygonShape();
         groundBox.dispose();
 
+    }
+
+    public World getWorld(){
+        return world;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
     }
 }
